1e67f2762fa446804841dec126e307bf1846f6a3
maruchi1203 <jheo1203@gmail.com>
2026-08-19 17:44:55 +0900

    refactor: move exceptions into common.exception package 2

--- backend/src/main/java/com/lonelytracker/backend/user/CategoryNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.lonelytracker.backend.user;
-
-import com.lonelytracker.backend.common.NotFoundException;
-
-public class CategoryNotFoundException extends NotFoundException {
-    public CategoryNotFoundException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/lonelytracker/backend/user/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.lonelytracker.backend.user;
-
-import com.lonelytracker.backend.common.NotFoundException;
-
-public class UserNotFoundException extends NotFoundException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
